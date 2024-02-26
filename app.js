1fe06ed3fa7a9ec8ac6095e7d7c36d4f08452953
smelyiAis <124904635+smelyiAis@users.noreply.github.com>
2024-02-27 02:09:15 +0600

    Add main files

---
+++ app.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+
+const { User, Track, Quiz } = require("./src/database/mongodb");
+const { login, signup } = require("./src/auth/auth");
+const { ensureAuthenticated, ensureUnauthenticated, ensureAdmin } = require("./src/middleware/middleware");
+const {
+    adminControllerGetUsers,
+    adminControllerDeleteUser,
+    adminControllerEditUser,
+    adminControllerEditUserPost,
+} = require("./src/admin/adminControllerUsers");
+const {
+    adminControllerAddTracks,
+    adminControllerDeleteTrack,
+    adminControllerEditTrack,
+    adminControllerEditTrackPost,
+    adminControllerGetTracks,
+} = require("./src/admin/adminControllerTracks");
+const { getFormulaOneNews } = require("./src/api/newsapi");
+const { getAllDrivers, getAllTeams } = require("./src/api/formulaoneapi");
+const { bonusPost, bonusGet } = require("./src/bonus/bonus");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: "fernandoalonso", resave: false, saveUninitialized: true }));
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+    const language = req.query.language;
+    const user = req.session?.user;
+    const tracks = await Track.find();
+    res.render("index", { user, tracks, language });
+});
+
+app.get("/news", async (req, res) => {
+    const language = req.query.language;
+    const user = req.session?.user;
+    const news = await getFormulaOneNews(language);
+    res.render("news", { user, news, language: language ?? "en" });
+});
+
+app.get("/drivers", async (req, res) => {
+    const user = req.session?.user;
+    const drivers = await getAllDrivers();
+    res.render("drivers", { user, drivers });
+});
+
+app.get("/constructors", async (req, res) => {
+    const user = req.session?.user;
+    const teams = await getAllTeams();
+    res.render("constructors", { user, constructors: teams });
+});
+
+app.get("/bonus", ensureAuthenticated, bonusGet);
+
+app.post("/bonus", ensureAuthenticated, bonusPost);
+
+app.get("/login", ensureUnauthenticated, async (req, res) => {
+    return res.render("login", { user: null, error: null, success: false });
+});
+
+app.get("/signup", ensureUnauthenticated, async (req, res) => {
+    return res.render("signup", { user: null, error: null });
+});
+
+app.post("/auth/login", ensureUnauthenticated, login);
+
+app.post("/auth/signup", ensureUnauthenticated, signup);
+
+app.get("/logout", ensureAuthenticated, async (req, res) => {
+    req.session.destroy();
+    res.redirect("/");
+});
+
+// Admin routes
+app.get("/admin", ensureAdmin, (req, res) => {
+    res.render("admin", { user: req.session?.user });
+});
+
+app.get("/admin/users", ensureAdmin, adminControllerGetUsers);
+app.get("/admin/users/delete/:id", ensureAdmin, adminControllerDeleteUser);
+app.get("/admin/users/edit/:id", ensureAdmin, adminControllerEditUser);
+app.post("/admin/users/edit/:id", ensureAdmin, adminControllerEditUserPost);
+
+app.get("/admin/tracks", ensureAdmin, adminControllerGetTracks);
+app.get("/admin/tracks/delete/:id", ensureAdmin, adminControllerDeleteTrack);
+app.get("/admin/tracks/edit/:id", ensureAdmin, adminControllerEditTrack);
+app.post("/admin/tracks/edit/:id", ensureAdmin, adminControllerEditTrackPost);
+app.post("/admin/tracks/add", ensureAdmin, adminControllerAddTracks);
+
+app.listen(3000, "0.0.0.0", () => {
+    console.log("Server is running on port 3000");
+});
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Final Project for WEB Technologies 2 (Backend) on Node.js (Express.js)
+
+# Project ==> "Formula House" - is an informational portal for Formula One Championship Limited fans
+
+<br>
+
+# How to run project?
+
+-   Clone the repository using `git clone`
+-   Enter the directory using `cd sultan-backend`
+-   Install required dependencies `npm i`
+-   Run the server using `npm run dev`
+
+### Note: this is server side rendered website, thus you don't need to run client separately.
+
+-   The server will run on port 3000 (can be changed in app.js file's end)
+
+# Used dependencies
+
+-   nodemon
+-   ejs
+-   bcrypt
+-   express
+-   express-session
+-   mongoose
+-   xml2js
+
+# Used API's
+
+-   News API by [NEWSAPI.ORG](https://newsapi.org/)
+-   Formula One Championship Limited API by Postman's User [LINK](https://documenter.getpostman.com/view/11586746/SztEa7bL#47c80b81-bf5d-4f84-8118-a80bee1631c4)
+
+# Thanks for your attention, don't forget to put 110% for this assignment :D
+
+-   Admin user: {login: 'Aisultan', password: '123123123'}
