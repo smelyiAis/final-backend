ba50b80face317a95dac78f30b3fecf43aa88673
Yelnur Abdrakhmanov <124904635+smelyiAis@users.noreply.github.com>
2024-02-27 02:54:55 +0600

    Add track edit

--- src/admin/adminControllerTracks.js
+++ src/admin/adminControllerTracks.js
@@ -27,13 +27,14 @@ async function adminControllerEditTrack(req, res) {
         return res.render("admin_tracks", { tracks, user: admin, error: "Track not found!", success: false });
     }
 
+    console.log(track);
+
     return res.render("admin_track_edit", { user: req.session?.user, track });
 }
 
 async function adminControllerEditTrackPost(req, res) {
     const trackId = req.params.id;
-    const { name, name_ru, description, description_ru, country, country_ru, city, city_ru, length, corners, attitude_difference } =
-        req.body;
+    const { name, name_ru, description, description_ru, country, country_ru, city, city_ru, length, corners, attitude_difference } = req.body;
     const tracks = await Track.find();
 
     try {
@@ -51,29 +52,15 @@ async function adminControllerEditTrackPost(req, res) {
             attitude_difference,
         });
     } catch (error) {
-        return res.render("admin_tracks", { user: req.session?.user, error: "Error editing track", success: false });
+        return res.render("admin_tracks", { user: req.session?.user, error: "Error editing track", success: false, tracks });
     }
 
-    return res.render("admin_tracks", { user: req.session?.user, error: null, success: "Track has been updated" });
+    return res.render("admin_tracks", { user: req.session?.user, error: null, success: "Track has been updated", tracks });
 }
 
 async function adminControllerAddTracks(req, res) {
-    let {
-        name,
-        name_ru,
-        description,
-        description_ru,
-        country,
-        country_ru,
-        city,
-        city_ru,
-        length,
-        corners,
-        attitude_difference,
-        link1,
-        link2,
-        link3,
-    } = req.body;
+    let { name, name_ru, description, description_ru, country, country_ru, city, city_ru, length, corners, attitude_difference, link1, link2, link3 } =
+        req.body;
     const tracks = await Track.find();
     try {
         length = parseInt(length);
